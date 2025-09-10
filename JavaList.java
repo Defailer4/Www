74d04492ccaad6b5be2615a3c26f4f33c27b18c9
D3ffailer <100451419+Defailer4@users.noreply.github.com>
2025-09-10 14:10:41 +0300

    JavaList.java

--- JavaList.java
+++ JavaList.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class App {
-    public static void main(String[] args) {
-        List<String> list = new ArrayList<>(Arrays.asList("one", "two", "three"));
-
-        list = List.of("1", "2", "3");
+public class Main {
+    public static void main(String[ ] args) {
+        int[] numbers = {9, 3, 5, 6, 7};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
     }
 }
